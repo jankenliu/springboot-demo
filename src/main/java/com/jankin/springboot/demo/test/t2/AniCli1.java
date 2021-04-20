d7a225a0324749e7885c35d89ba6281e43ffd43c
p_pyyliu <p_pyyliu@tencent.com>
2021-04-20 11:13:33 +0800

    2

---
+++ src/main/java/com/jankin/springboot/demo/test/t2/AniCli1.java
@@ -0,0 +1,22 @@
+package com.jankin.springboot.demo.test.t2;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AniCli1 extends AniAbstract{
+    int b;
+
+    public int getAVal(){
+        return a;
+    }
+
+    public int getBVal(){
+        return b;
+    }
+
+
+    public AniCli1() {
+        System.out.println("---------");
+        System.out.println("2343");
+    }
+}
